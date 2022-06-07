9ae46ddadcc34008474c0754eb6152b32a49e830
Christopher Debove <christopher.debove@toucantoco.com>
2022-06-07 14:26:57 +0200

    fix(VueInReact): fix typing for VueInReact API

--- src/resolvers/React.tsx
+++ src/resolvers/React.tsx
@@ -1,11 +1,12 @@
+import type { ExtractPropTypes } from '@vue/composition-api';
 import React, { type ReactNode } from 'react';
-import { Component } from 'vue';
+import type { Component } from 'vue';
 import { VueWrapper } from '../wrappers/Vue';
 
 type VueInReactProps<P> = {
   on?: Record<string, any>;
   children?: ReactNode;
-} & P;
+} & ExtractPropTypes<P>;
 
 export function VueInReact<P extends Record<string, any>>(component: Component<any, any, any, P>) {
   return (props: VueInReactProps<P>) => <VueWrapper component={component} {...props} />;
